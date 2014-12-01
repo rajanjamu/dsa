4a3e40d866fac05adcf361f1cf9bed0bd8f66f48
Rajan Gupta <rajanjamu@gmail.com>
2014-12-01 11:43:13 +0530

    created merge_sort.rb

---
+++ merge_sort.rb
@@ -0,0 +1,21 @@
+# SORTING ARRAY USING MERGE-SORT ALGORITHM
+# AUTHOR: RAJAN GUPTA
+# CREATED: 1 DEC 2014
+
+# Split array and return two arrays
+def split_array(array)
+end
+
+# Sort the input array
+def sort_array(array)
+end
+
+# Merge two arrays and return merged array
+def merge_array(array1, array2)
+end
+
+# Input variable
+arr = [3,7,1,9,2,67,0]
+
+# Printing out the result
+puts merge_sort_array(arr)
